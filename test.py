ef1d2b1be0390323e78bc229abc9eba9a9c5658a
RS20242024 <161766916+RS20242024@users.noreply.github.com>
2025-10-21 11:02:08 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-print(" Hello, Github")
-print(" test if it worked")
+import numpy as np
+import matplotlib.pyplot as plt
 
-print(" test for laptop access")
+image_array = np.random.rand(100, 100) * 255 
+image_array = image_array.astype(np.uint8) # Ensure correct data type for image display
+plt.imshow(image_array, cmap='gray') # 'gray' colormap for grayscale images
+plt.colorbar() # Optional: add a color bar for intensity reference
+plt.title("NumPy Array as Image")
+plt.show()
